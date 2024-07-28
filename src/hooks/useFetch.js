8eeda475e87e56f7c23beb6b2de79cc9af24500a
Aadil <90678881+Aadilkhan12@users.noreply.github.com>
2024-07-28 11:56:30 +0530

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 
 export const useFetch = (apiPath, queryTerm="") => {
+    const api_key ="b80d59c33d6d57ed9c7e3713f91c188a";
     const [data, setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${api_key}&query=${queryTerm}`
 
     useEffect(() => {
         async function fetchMovies(){
